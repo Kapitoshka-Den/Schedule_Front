cb71904523abf4e8191ea1beb0320de2e470e28d
Denis <turkov_d_v@mail.ru>
2022-03-30 17:26:59 +0300

    schedule front with minimal style and group

--- src/Comoponents/SchedulPage.tsx
+++ src/Comoponents/SchedulPage.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import  './SchedulPage'
+import  './SchedulePage.css'
 const SchedulePage = (props:any) =>{
     
     
     return(
         <div className='schdedulePage'>
+            
             {props.scheduleItems.map((item: { id: React.Key | null | undefined; Date: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; LessonNumber: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; Group: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; Subject: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; Auditories: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; Teachers: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; }) => (
                 <div key={item.id} className="oneSubjectInSchedule">
                     <h2>{item.Date} - {item.LessonNumber} урок</h2> 
                     <p>{item.Subject}</p>
+                    <p><b>Группа</b> : {item.Group}</p>
                     <p><b>Аудиории</b> : {item.Auditories}</p>
                     <p><b>Преподаватели</b> : {item.Teachers}</p>
                 </div>
